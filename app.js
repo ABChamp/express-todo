3274484651fd58069df9fe829b69eba81fd29f2d
Champ Kongsilp <56160232@go.buu.ac.th>
2018-09-28 15:25:49 +0700

    update todo app

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ var cors = require('cors')
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use('/todo', todo);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
