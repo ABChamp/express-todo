2414aba54630835dfa422b3e27e60a78b29fea2c
Champ Kongsilp <56160232@go.buu.ac.th>
2018-09-28 16:28:10 +0700

    update / for display

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const todo = require('./todo.js');
-var port = process.env.PORT || 3000; //
+var port = process.env.PORT || 3001; //
 var cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/todo', todo);
-
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
